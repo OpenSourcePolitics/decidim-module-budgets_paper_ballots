446a35498c6334df81d4741b7017a7351386c5f6
stephanie rousset <strabon@club-internet.fr>
2026-03-10 09:22:36 +0100

    fix: replace enable_reloading by cache_classes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,11 +8,12 @@ require "active_support/core_ext/integer/time"
 # and recreated between test runs. Don't rely on the data there!
 
 # setting added to fix error on github CI
-# /home/runner/work/decidim-module-budgets_paper_ballots/decidim-module-budgets_paper_ballots/vendor/bundle/ruby/3.3.0/gems/spring-4.4.2/lib/spring/application.rb:110:in `block in preload': Spring reloads, and therefore needs the application to have reloading enabled.
+# /home/runner/work/decidim-module-budgets_paper_ballots/decidim-module-budgets_paper_ballots/vendor/bundle/ruby/3.3.0/gems/spring-4.4.2/lib/spring/application.rb:110:in
+# `block in preload': Spring reloads, and therefore needs the application to have reloading enabled.
 # Please, set config.enable_reloading to true in config/environments/test.rb.
 #  (RuntimeError)
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.enable_reloading = true
+  config.cache_classes = false
 end
